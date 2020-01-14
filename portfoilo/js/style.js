201d3b7c697ef94672c313294337580095b8495b
hongli-95 <59783934+hongli-95@users.noreply.github.com>
2020-01-13 19:01:53 -0700

    Delete style.js

--- portfoilo/js/style.js
+++
@@ -1,8 +0,0 @@
-// <!-- a little js and jQuery for the navbar visual effect (Need to learn more about this)
-$(function () {
-    $(document).scroll(function() {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scroll", $(this).scrollTop() > $nav.height());
-    });
-});
-
