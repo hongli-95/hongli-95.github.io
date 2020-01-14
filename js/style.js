af22e20eff73612e2b09706ec6cc9155e30c88e4
hongli-95 <59783934+hongli-95@users.noreply.github.com>
2020-01-13 21:05:24 -0700

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,8 +1,8 @@
-// <!-- a little js and jQuery for the navbar visual effect (Need to learn more about this)
-$(function () {
-    $(document).scroll(function() {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scroll", $(this).scrollTop() > $nav.height());
-    });
-});
+// // <!-- a little js and jQuery for the navbar visual effect (Need to learn more about this)
+// $(function () {
+//     $(document).scroll(function() {
+//         var $nav = $("#mainNavbar");
+//         $nav.toggleClass("scroll", $(this).scrollTop() > $nav.height());
+//     });
+// });
 
